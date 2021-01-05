ec361592bed9aed4e66cae2449c1536f7343ac41
RUBEN BLENICIO TAVARES SILVA <bleniciosilva@gmail.com>
2021-01-05 18:08:57 -0300

    Número de jobs
    Foi implementado o cálculo do número de jobs (com sucesso/falha) para
    cada merge request
    
    closes #9

--- gitlab-listener/metric/number_jobs.py
+++ gitlab-listener/metric/number_jobs.py
@@ -1,5 +1,17 @@
-class NumberJobs:
-    def __init__(self,gitlab):
-        self.gitlab = gitlab
-    def compute(self, project, start_date, end_date):
-        print("Número de jobs")
+def compute_jn(project, merge_requests):
+    for merge_request in merge_requests:
+        pipelines = merge_request.pipelines()
+
+        success_jobs_count = 0
+        failed_jobs_count = 0
+        for pipeline in pipelines:
+            pipeline = project.pipelines.get(pipeline['id'])
+            jobs = pipeline.jobs.list()
+
+            for job in jobs:
+                if job.status == 'success':
+                    success_jobs_count += 1
+                elif job.status == 'failed':
+                    failed_jobs_count += 1
+
+        print(success_jobs_count, failed_jobs_count)
