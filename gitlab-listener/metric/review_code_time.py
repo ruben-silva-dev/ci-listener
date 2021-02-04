7b2d8a6047316b9454be110cd649949a8f9e653f
RUBEN BLENICIO TAVARES SILVA <bleniciosilva@gmail.com>
2021-02-04 11:13:15 -0300

    Métrica do tempo de revisão do código
    Foi implementado o cálculo da métrica de tempo de revisão do código.
    
    closes #20

--- gitlab-listener/metric/review_code_time.py
+++ gitlab-listener/metric/review_code_time.py
@@ -1,6 +1,27 @@
-class ReviewCodeTime:
-    def __init__(self, gitlab):
-        self.gitlab = gitlab
+import re
+from datetime import datetime
 
-    def compute(self, project, start_date, end_date):
-        print("Review Time Code")
+datetime_format = '%Y-%m-%dT%H:%M:%S.%fZ'
+bot_codacy = "codacy-bot|Codacy"
+
+
+def compute_rct(merge_requests):
+    for merge_request in merge_requests:
+        notes = []
+
+        for note in merge_request.notes.list():
+            test_bot = re.findall(bot_codacy, note.body)
+            if not note.system and not test_bot:
+                notes.append(note)
+
+        if notes:
+            notes.sort(key=pipeline_sort)
+
+            start_datetime = datetime.strptime(notes[0].created_at, datetime_format)
+            end_datetime = datetime.strptime(notes[len(notes)-1].created_at, datetime_format)
+
+            print(end_datetime - start_datetime)
+
+
+def pipeline_sort(note):
+    return note.created_at
