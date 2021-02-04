82282a43778fcd16ba193c038198779cf8211444
RUBEN BLENICIO TAVARES SILVA <bleniciosilva@gmail.com>
2021-02-04 09:37:16 -0300

    Métrica de latência de ci
    Foi implementado o cálculo da métrica de latência de ci por merge
    request.
    
    closes #19

--- gitlab-listener/metric/ci_latency.py
+++ gitlab-listener/metric/ci_latency.py
@@ -1,6 +1,21 @@
-class CiLatency:
-    def __init__(self, gitlab):
-        self.gitlab = gitlab
+from datetime import datetime
 
-    def compute(self, project, start_date, end_date):
-        print("Latência de integração contínua")
+datetime_format = '%Y-%m-%dT%H:%M:%S.%fZ'
+
+
+def compute_cl(merge_requests):
+    for merge_request in merge_requests:
+        start_datetime = datetime.strptime(merge_request.created_at, datetime_format)
+
+        pipelines = merge_request.pipelines()
+        pipelines.sort(key=pipeline_sort, reverse=True)
+
+        for pipeline in pipelines:
+            if pipeline['status'] == 'failed' or pipeline['status'] == 'success':
+                end_datetime = datetime.strptime(pipeline['created_at'], datetime_format)
+                print(end_datetime - start_datetime)
+                break
+
+
+def pipeline_sort(pipeline):
+    return pipeline['created_at']
